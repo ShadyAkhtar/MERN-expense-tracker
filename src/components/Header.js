4cb2d196c9aef9e5d1fe257b0ae3c7bcd9f0c45d
Shadab <akhtarshadab415@gmail.com>
2021-06-23 20:26:32 +0530

    Layout, Calculation, Add, Delete

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div>
-            <h2>Expense Header</h2>
+            <h2>Expense Tracker</h2>
         </div>
     )
 }
